97cc444a4cfc1263ff3c7eaaf37b7678d63b54a4
Shubham Sharma <shubhamicsharma@gmail.com>
2024-03-21 21:45:12 +0530

    Assignment Completed

--- src/components/Hero/Hero3.jsx
+++ src/components/Hero/Hero3.jsx
@@ -1,29 +1,24 @@
-'use client'
-
 import PropTypes from 'prop-types';
 import React from 'react';
 import './Hero.scss';
 import perser from 'html-react-parser';
 import SocialLinks2 from '../SocialLinks/SocialLinks2';
-import WaterWave from 'react-water-wave';
 
 const Hero3 = ({ data, socialData }) => {
-  const { name, subTitle, avatar : {url}, title } = data;
+  const { name, subTitle, avatar: { url }, title } = data;
   return (
-    <WaterWave id="home" className="st-hero st-style2 st-bg st-dynamic-bg st-ripple-version" imageUrl= "/images/background/hero-bg-1.jpg">
-      {() => (
-        <div className="container">
-          <div className="st-hero-text">
-            {url && <div className="st-author" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
-              <img src={url} alt="Author Image" />
-            </div>}
-            {name && <h1 data-aos="fade-up" data-aos-duration="800" data-aos-delay="300">Hi, I am <span>{perser(name)}</span></h1>}
-            {title && subTitle && <p data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">I am a {title}. {perser(subTitle)}</p>}
-            <SocialLinks2 data={socialData} />
-          </div>
+    <div id="home" className="st-hero st-style2 st-bg st-dynamic-bg st-ripple-version" style={{ backgroundImage: "url('/images/background/hero-bg-1.jpg')" }}>
+      <div className="container">
+        <div className="st-hero-text">
+          {url && <div className="st-author" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
+            <img src={url} alt="Author Image" />
+          </div>}
+          {name && <h1 data-aos="fade-up" data-aos-duration="800" data-aos-delay="300">Hi, I am <span>{perser(name)}</span></h1>}
+          {title && subTitle && <p data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">I am a {title}. {perser(subTitle)}</p>}
+          <SocialLinks2 data={socialData} />
         </div>
-      )}
-    </WaterWave>
+      </div>
+    </div>
   )
 }
 
